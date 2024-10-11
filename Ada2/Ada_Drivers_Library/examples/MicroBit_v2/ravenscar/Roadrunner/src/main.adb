3ba21ff786378db7b7944c9f018f690bf2f1ad0f
Philip <philip91novak@gmail.com>
2024-10-11 14:16:21 +0200

    Updated code to two sensors

--- Ada2/Ada_Drivers_Library/examples/MicroBit_v2/ravenscar/Roadrunner/src/main.adb
+++ Ada2/Ada_Drivers_Library/examples/MicroBit_v2/ravenscar/Roadrunner/src/main.adb
@@ -7,14 +7,18 @@ use MicroBit;
 
 procedure Main is
    package sensor1 is new Ultrasonic(MB_P16, MB_P15);
+   package sensor2 is new Ultrasonic(MB_P14, MB_P13);
 
    Distance : Distance_cm := 0;
    run : Integer := 1;
    Last_Distance : Distance_cm := 0;
    Stabilize_Time : constant Duration := 0.2;  -- Time to stabilize before changing direction
+
+   Distance_2 : Distance_cm := 0;
 begin
    loop
       Distance := sensor1.Read;
+      Distance_2 := sensor2.Read;
 
       -- Log distance only if it changes to avoid slowing down
       if Distance /= Last_Distance then
@@ -22,12 +26,13 @@ begin
          Last_Distance := Distance;
       end if;
 
+
       -- Brief pause before switching directions to stabilize sensor
       if (Distance < 20) and (Distance /= 0) and (run = 1) then
          MotorDriver.Drive(Stop, (0, 0, 0, 0)); -- Stop momentarily
          delay Stabilize_Time;
          run := 0;  -- Switch to reverse
-      elsif (Distance >= 21) and (run = 0) then
+      elsif (Distance >= 21) and (run = 0) and (Distance_2 < 21) and (Distance_2 /= 0) then
          MotorDriver.Drive(Stop, (0, 0, 0, 0)); -- Stop momentarily
          delay Stabilize_Time;
          run := 1;  -- Switch to forward
