1039ecdda8fea613e92e8ff6c82c0379923cd2b6
Philip <philip91novak@gmail.com>
2024-10-30 10:15:36 +0100

    Rydding

--- Ada2/Ada_Drivers_Library/examples/MicroBit_v2/ravenscar/Roadrunner/src/main.adb
+++ Ada2/Ada_Drivers_Library/examples/MicroBit_v2/ravenscar/Roadrunner/src/main.adb
@@ -5,27 +5,51 @@ with MicroBit.MotorDriver; use MicroBit.MotorDriver;
 with DFR0548;
 use MicroBit;
 
+
+
+
+
 procedure Main is
-   package sensor1 is new Ultrasonic(MB_P16, MB_P15);
-   package sensor2 is new Ultrasonic(MB_P14, MB_P13);
+   package sensor1 is new Ultrasonic(MB_P1, MB_P0);
+   package sensor2 is new Ultrasonic(MB_P8, MB_P2);
+   package sensor3 is new Ultrasonic(MB_P13, MB_P12);
+
+
+   run : Integer := 1;
 
    Distance : Distance_cm := 0;
-   run : Integer := 1;
    Last_Distance : Distance_cm := 0;
-   Stabilize_Time : constant Duration := 0.2;  -- Time to stabilize before changing direction
-
+   
    Distance_2 : Distance_cm := 0;
+   Last_Distance_2 : Distance_cm := 0;
+
+   Distance_Rear : Distance_cm := 0;
+   Last_Distance_Rear : Distance_cm := 0;
+
+
+   Stabilize_Time : constant Duration := 0.2;  -- Time to stabilize before changing direction
 begin
    loop
       Distance := sensor1.Read;
       Distance_2 := sensor2.Read;
+      Distance_Rear := sensor3.Read;
 
       -- Log distance only if it changes to avoid slowing down
       if Distance /= Last_Distance then
-         Put_Line ("Distance: " & Distance_cm'Image(Distance));
+         Put_Line ("Right: " & Distance_cm'Image(Distance));
          Last_Distance := Distance;
       end if;
 
+      -- Log distance only if it changes to avoid slowing down
+      if Distance_2 /= Last_Distance_2 then
+         Put_Line ("Left: " & Distance_cm'Image(Distance_2));
+         Last_Distance_2 := Distance_2;
+      end if;
+
+      if Distance_Rear /= Last_Distance_Rear then
+         Put_Line ("Rear: " & Distance_cm'Image(Distance_Rear));
+         Last_Distance_Rear := Distance_Rear;
+      end if;
 
       -- Brief pause before switching directions to stabilize sensor
       if (Distance < 20) and (Distance /= 0) and (run = 1) then
@@ -36,16 +60,28 @@ begin
          MotorDriver.Drive(Stop, (0, 0, 0, 0)); -- Stop momentarily
          delay Stabilize_Time;
          run := 1;  -- Switch to forward
+      
       end if;
 
       -- Run the motor based on the direction
       if run = 1 then
          MotorDriver.Drive(Forward, (4095, 4095, 4095, 4095));
       elsif run = 0 then
-         MotorDriver.Drive(Rotating_Right, (4095, 4095, 4095, 4095));
+         MotorDriver.Drive(Backward, (4095, 4095, 4095, 4095));
       end if;
 
       delay 0.05;  -- Allow sensor to refresh
+
+      for I in DFR0548.Degrees range 0..180 loop
+         MotorDriver.Servo(1,I);
+         delay 0.006; --20 ms
+      end loop;
+
+      for I in reverse DFR0548.Degrees range 0..180 loop
+         MotorDriver.Servo(1,I);
+          delay 0.006; --20 ms
+      end loop;
+
    end loop;
 
 end Main;
